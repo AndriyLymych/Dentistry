1eb71616295d32c88613987e430425fdbbf6ffec
AndriyLymych <lymych1994@gmail.com>
2020-01-24 14:26:38 +0200

    commit

--- migrations/20200124083154-basic-dates.js
+++ migrations/20200124083154-basic-dates.js
@@ -4,6 +4,105 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
 
     await queryInterface.sequelize.query(
+        `INSERT INTO gender(
+                    label
+                 ) 
+                  VALUE (
+                     'male'
+                 )`
+    );
+    await queryInterface.sequelize.query(
+        `INSERT INTO gender(
+                    label
+                 ) 
+                  VALUE (
+                     'female'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_role(
+                    label
+                 ) 
+                  VALUE (
+                     'admin'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_role(
+                    label
+                 ) 
+                  VALUE (
+                     'patient'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_role(
+                    label
+                 ) 
+                  VALUE (
+                     'doctor'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_status(
+                    label
+                 ) 
+                  VALUE (
+                     'active'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_status(
+                    label
+                 ) 
+                  VALUE (
+                     'blocked'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_speciality(
+                    label
+                 ) 
+                  VALUE (
+                     'orthodontist'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_speciality(
+                    label
+                 ) 
+                  VALUE (
+                     'orthopaedist'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_speciality(
+                    label
+                 ) 
+                  VALUE (
+                     'therapist'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+        `INSERT INTO user_speciality(
+                    label
+                 ) 
+                  VALUE (
+                     'surgeon'
+                 )`
+    );
+
+    await queryInterface.sequelize.query(
+
         `INSERT INTO user(
                     email,
                     password, 
