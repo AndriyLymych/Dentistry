c491671690f61ce3b51e489aceea48fa39b364e4
AndriyLymych <lymych1994@gmail.com>
2020-02-25 16:55:30 +0200

    commit

--- services/emailService/sendRememberRecordMail.service.js
+++ services/emailService/sendRememberRecordMail.service.js
@@ -10,33 +10,26 @@ module.exports = async () => {
     const {transport} = mailerTransport();
 
     const records = JSON.parse(await requestPromise.get(HOST + ':3000/receptions'));
+    records.forEach(record => {
+        let date = new Date(record.date);
+        date.setDate(date.getDate() - 1);
+        record.date = date;
+        while (record.count_mail <= 1) {
+            cron.schedule('* * * * * *', () => {
+                if (record.date) {
 
-    cron.schedule('* * * * * *', () => {
-        records.forEach(async record => {
-            let date = new Date(record.date);
-            date.setDate(date.getDate() - 1);
-            record.date = date;
-            if (record.date) {
-                try {
-                    while (record.count_mail <= 1) {
-                        await transport.sendMail({
-                            from: `Simstomat ${EMAIL_DATES.EMAIL} `,
-                            to: record.email,
-                            subject: 'Нагадування',
-                            html: `<p>Добрий день, ${record.name}. Нагадуємо що у Вас завтра прийом у стоматології "Simstomat"!</p>`
-                        })
-
-                        record.count_mail++
-
-                    }
-                } catch (e) {
-                    console.log(e);
+                    transport.sendMail({
+                        from: `Simstomat ${EMAIL_DATES.EMAIL} `,
+                        to: record.email,
+                        subject: 'Нагадування',
+                        html: `<p>Добрий день, ${record.name}. Нагадуємо що у Вас завтра прийом у стоматології "Simstomat"!</p>`
+                    })
 
                 }
-            }
-        })
-
-    });
+            });
+            record.count_mail++
+        }
+    })
 
 
 };
