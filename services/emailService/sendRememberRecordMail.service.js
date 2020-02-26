debf57134756bf0c7df11f426a65b9e95623c6f5
AndriyLymych <lymych1994@gmail.com>
2020-02-26 10:25:24 +0200

    commit

--- services/emailService/sendRememberRecordMail.service.js
+++ services/emailService/sendRememberRecordMail.service.js
@@ -4,6 +4,7 @@ const requestPromise = require('request-promise');
 const {EMAIL_DATES} = require('../../constant');
 const {mailerTransport} = require('../../helpers');
 const {HOST} = require('../../config/configs');
+const {incrementCountMail} = require('../receptionService');
 
 module.exports = async () => {
 
@@ -11,14 +12,13 @@ module.exports = async () => {
 
     const records = JSON.parse(await requestPromise.get(HOST + ':3000/receptions'));
     records.forEach(record => {
+        let {id, date, count_mail, name} = record;
+        date = new Date(date);
+        date.setDate(date.getDate() - 1);
 
-        let date = new Date(record.date); // ШО ЗА НАХУЙ ?
-        date.setDate(date.getDate() - 1); // ШО ЗА НАХУЙ ?
-        record.date = date; // ШО ЗА НАХУЙ ?
-
-        while (record.count_mail <= 1) {
+        while (count_mail <= 1) {
             cron.schedule('* * * * * *', () => {
-                if (record.date) { // ШО ЗА НАХУЙ ?
+                if (record.date) {
 
                     transport.sendMail({
                         from: `Simstomat ${EMAIL_DATES.EMAIL} `,
@@ -29,7 +29,7 @@ module.exports = async () => {
 
                 }
             });
-            record.count_mail++
+            incrementCountMail({count_mail: 1})
         }
     })
 
--- services/receptionService/index.js
+++ services/receptionService/index.js
@@ -1,1 +1,2 @@
 module.exports.getAllReceptionRecords = require('./getAllReceptionRecords.service');
+module.exports.incrementCountMail = require('./incrementCountMail.service');
